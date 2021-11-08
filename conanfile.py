4c2d9c60bc75a24444b07eb7f28c1a3b4892d4b5
Dvir Yitzchaki <dyitzchaki@verizonmedia.com>
2021-11-08 19:55:10 +0000

    add conan long imports bug

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile
+from datetime import datetime
 
 class Bug(ConanFile):
   requires = "boost/1.76.0"
@@ -7,4 +8,6 @@ class Bug(ConanFile):
   }
 
   def imports(self):
+    self.output.info("starting import at {}".format(datetime.now().isoformat()))
     self.copy("*.so*", dst="lib", src="@libdirs")
+    self.output.info("finished import at {}".format(datetime.now().isoformat()))
